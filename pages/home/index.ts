eaf6edfc01ad39f52cb97f6108ec45e1379890ac
Lucas Bastidas <lucaasbastidas@hotmail.com>
2023-01-11 13:29:44 -0300

    update project section

--- pages/home/index.ts
+++ pages/home/index.ts
@@ -13,6 +13,90 @@ const sequalizeImg = require("../../assets/tecnologias-iconos/sequelize.svg");
 const gitImg = require("../../assets/tecnologias-iconos/git.png");
 const nextJsIcon = require("../../assets/tecnologias-iconos/nextjs-icon.svg");
 
+const projects = [
+	{
+		title: "Movie-app",
+		description:
+			"App que podemos usar vía la terminal. Esta app busca entre películas que tengo guardadas en un archivo JSON. Utilicé los conceptos básicos de la programación para articular un pequeño motor que filtra y busca nuestras películas favoritas. Para esto utilicé JavaScript sobre Node.js y conceptos como collections para organizar la data de nuestra app.",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/fee47c8a4261ae95fc2d20ba8d5b8c0927daa419-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/dwf-m2-desafio",
+		demoUrl: "https://github.com/LucasBastidas/dwf-m2-desafio",
+	},
+	{
+		title: "Web personal interactiva",
+		description:
+			"Web responsive usando HTML, CSS y JavaScript. El sitio web mostraría mi información personal y profesional usando distintas páginas. Profundicé en CSS y en formas ordenadas de utilizarlo como BEM. También utilicé un sistema de gestión de contenidos para alimentar la web a través de una API.",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/93df2dc53a5e6bea481650c9c9f0f9896bafbc88-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/desafio-final-dwf-m4",
+		demoUrl: "https://lucasbastidas.github.io/desafio-final-dwf-m4/",
+	},
+	{
+		title: "Piedra, papel o tijera",
+		description:
+			"Webapp para jugar a piedra, papel o tijera basada web-components. Desarrollé una SPA basada en una arquitectura orientada a componentes. Además basé toda la arquitectura en un router y un state general de la app desarrollada 100% en vanilla JavaScript. Utilicé TypeScript en conjunto con Parcel..",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/befad7fb1b871ecf5c9160fc82510de4dc1a8ea4-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/desafio-m5",
+		demoUrl: "https://lucasbastidas.github.io/desafio-m5/",
+	},
+	{
+		title: "Piedra, papel o tijera (Versión online)",
+		description:
+			"WebApp para jugar a piedra, papel o tijera entre dos participantes conectados al servidor. Además de escalar la UI que ya tenía, cree una API Rest que nos permite generar game rooms. Para esto utilicé Firebase Realtime Database para mantener la comunicación entre jugadores. El servidor en este caso es Heroku.",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/883de42c574af95fe1e74cbbb642a62c3b1bb90f-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/dwf-m6-desafio-final",
+		demoUrl: "https://piedra-papel-tijeras-v2.onrender.com/",
+	},
+	{
+		title: "Chat-Rooms",
+		description:
+			"Webapp sencilla donde se pueden crear y unirse a chatRooms utilizando RealTimeDataBase de firebase.",
+		imageUrl:
+			"https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		repoUrl: "https://github.com/LucasBastidas/chatroom-app",
+		demoUrl: "https://chat-rooms-webapp.onrender.com/",
+	},
+	{
+		title: "Directorio de mascotas",
+		description:
+			"Webapp donde podrás reportar mascotas perdidas. Exploré patrones conocidos como MVC y REST además de entender cómo funcionan las bases de datos relacionales.Además diseñé un sistema de autenticación e integré servicios como Algolia y Mapbox para integrar geolocalización en el sistema.",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/ceb4612f5c545b4eb91213708e9f9dce1d58bda2-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/lost-pets-app-v1",
+		demoUrl: "https://lost-pets-webapp-v1.onrender.com",
+	},
+	{
+		title: "Directorio de mascotas v2 (React)",
+		description:
+			"Planteé un nuevo front para la aplicación de mascotas perdidas. El foco era practicar con React. Se  profundiza en la arquitectura basada en capas y el uso de componentes de forma escalable. Empecé a usar hooks y a entender los beneficios de la programación reactiva. Esta vez utilicé Firebase como host.",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/bbfa437c8a52749a2c02f22abfc8af63c0fee5ac-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/lost-pets-v2-react",
+		demoUrl: "https://lost-pets-app-v2-react.firebaseapp.com/",
+	},
+	{
+		title: "E-Commerce (Desarrollo frontend)",
+		description:
+			"Frontend de un e-commerce. El foco de este proyecto está en profundizar conceptos de React y sus herramientas como Next.js y Vercel. También trabajé una arquitectura basada en capas, server-side-rendering y la performance de la web.",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/aa8ec638489af74fd24e7b508833cf3a93b2a9b8-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/ecommerce-fe",
+		demoUrl: "https://ecommerce-fe-eosin.vercel.app/",
+	},
+	{
+		title: "E-Commerce (Desarrollo backend)",
+		description:
+			"Backend de un e-commerce usando MercadoPago como procesador de pagos. El foco de este desafío está en la escalabilidad. Para eso utilicé muchas bases de datos como Firebase, Airtable y Algolia para diseñar un backend basado en una API Rest robusta que integra a varias tecnologías y saca lo mejor de cada una.",
+		imageUrl:
+			"https://cdn.sanity.io/images/9ihg87o9/production/9c1e55ddb0b2c013774f6bdbbb6801bfdc6bcbb4-312x165.png",
+		repoUrl: "https://github.com/LucasBastidas/ecommerce-be",
+		demoUrl: "https://github.com/LucasBastidas/ecommerce-be",
+	},
+];
+
 class Home extends HTMLElement {
 	connectedCallback() {
 		this.render();
@@ -291,29 +375,13 @@ class Home extends HTMLElement {
 		<h2>Mis Proyectos</h2>
 		</div>
 		<div class="projects-cont">
-		<x-project-card title="Movie-app" imageUrl="https://cdn.sanity.io/images/9ihg87o9/production/fee47c8a4261ae95fc2d20ba8d5b8c0927daa419-312x165.png"  description="App que podemos usar vía la terminal. Esta app busca entre películas que tengo guardadas en un archivo JSON. Utilicé los conceptos básicos de la programación para articular un pequeño motor que filtra y busca nuestras películas favoritas. Para esto utilicé JavaScript sobre Node.js y conceptos como collections para organizar la data de nuestra app."url="https://github.com/LucasBastidas/dwf-m2-desafio" Url2="https://github.com/LucasBastidas/dwf-m2-desafio"></x-project-card>
-
-		<x-project-card title="Web personal interactiva" imageUrl="https://cdn.sanity.io/images/9ihg87o9/production/93df2dc53a5e6bea481650c9c9f0f9896bafbc88-312x165.png" description="Web responsive usando HTML, CSS y JavaScript. El sitio web mostraría mi información personal y profesional usando distintas páginas. Profundicé en CSS y en formas ordenadas de utilizarlo como BEM. También utilicé un sistema de gestión de contenidos para alimentar la web a través de una API."
-		url="https://github.com/LucasBastidas/desafio-final-dwf-m4" url2="https://lucasbastidas.github.io/desafio-final-dwf-m4/" ></x-project-card>
-
-		<x-project-card title="Piedra, papel o tijera" imageUrl="https://cdn.sanity.io/images/9ihg87o9/production/befad7fb1b871ecf5c9160fc82510de4dc1a8ea4-312x165.png" description="Webapp para jugar a piedra, papel o tijera basada web-components. Desarrollé una SPA basada en una arquitectura orientada a componentes. Además basé toda la arquitectura en un router y un state general de la app desarrollada 100% en vanilla JavaScript. Utilicé TypeScript en conjunto con Parcel.." url="https://github.com/LucasBastidas/desafio-m5" url2="https://lucasbastidas.github.io/desafio-m5/" >></x-project-card>
-
-
-		<x-project-card title="Chat-Room App" imageUrl="https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" description="Webapp sencilla donde se pueden crear y unirse a chatRooms utilizando RealTimeDataBase de firebase.
-		"url="https://github.com/LucasBastidas/chatroom-app" url2="https://m6-chat-rooms-app.herokuapp.com/" ></x-project-card>
 		
+		${projects
+			.map((r) => {
+				return `<x-project-card title="${r.title}" imageUrl="${r.imageUrl}" description="${r.description}" url="${r.repoUrl}" url2="${r.demoUrl}"></x-project-card>`;
+			})
+			.join("")}
 		
-		<x-project-card title="Piedra, papel o tijera (Versión online)" imageUrl="https://cdn.sanity.io/images/9ihg87o9/production/883de42c574af95fe1e74cbbb642a62c3b1bb90f-312x165.png" description="WebApp para jugar a piedra, papel o tijera entre dos participantes conectados al servidor. Además de escalar la UI que ya tenía, cree una API Rest que nos permite generar game rooms. Para esto utilicé Firebase Realtime Database para mantener la comunicación entre jugadores. El servidor en este caso es Heroku." url="https://github.com/LucasBastidas/dwf-m6-desafio-final" url2="https://piedra-papel-tijeras-v2.onrender.com/" ></x-project-card>
-		
-		
-		<x-project-card title="Directorio de mascotas" imageUrl="https://cdn.sanity.io/images/9ihg87o9/production/ceb4612f5c545b4eb91213708e9f9dce1d58bda2-312x165.png" description="Webapp donde podrás reportar mascotas perdidas. Exploré patrones conocidos como MVC y REST además de entender cómo funcionan las bases de datos relacionales.Además diseñé un sistema de autenticación e integré servicios como Algolia y Mapbox para integrar geolocalización en el sistema." url="https://github.com/LucasBastidas/lost-pets-app-v1" url2="https://lost-pets-webapp-v1.onrender.com" ></x-project-card>
-		
-		
-		
-		<x-project-card title="Directorio de mascotas v2 (React)" imageUrl="https://cdn.sanity.io/images/9ihg87o9/production/bbfa437c8a52749a2c02f22abfc8af63c0fee5ac-312x165.png" description="Planteé un nuevo front para la aplicación de mascotas perdidas. El foco era practicar con React. Se  profundiza en la arquitectura basada en capas y el uso de componentes de forma escalable. Empecé a usar hooks y a entender los beneficios de la programación reactiva. Esta vez utilicé Firebase como host." url="https://github.com/LucasBastidas/lost-pets-v2-react" url2="https://lost-pets-app-v2-react.firebaseapp.com/" ></x-project-card>
-
-		<x-project-card title="E-Commerece (Desarrollo Frontend)" imageUrl="https://cdn.sanity.io/images/9ihg87o9/production/aa8ec638489af74fd24e7b508833cf3a93b2a9b8-312x165.png" description="Frontend de un e-commerce. El foco de este proyecto está en profundizar conceptos de React y sus herramientas como Next.js y Vercel. También trabajé una arquitectura basada en capas, server-side-rendering y la performance de la web." url="https://github.com/LucasBastidas/ecommerce-fe" url2="https://ecommerce-fe-eosin.vercel.app/" ></x-project-card>
-
 
 		</div>
 		</section>
